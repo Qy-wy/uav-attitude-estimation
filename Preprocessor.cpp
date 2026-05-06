444fb323b752a7fea4be789db406fd29f6cde215
Qy-wy <lizaliyts@gmail.com>
2026-05-06 21:14:37 +0300

    Implement Preprocessor

---
+++ Preprocessor.cpp
@@ -0,0 +1,46 @@
+#include "Preprocessor.h"
+
+using namespace cv;
+
+Preprocessor::Preprocessor(const std::string& path)
+{
+    videoPath = path;
+}
+
+bool Preprocessor::loadFirstFrame()
+{
+    Mat firstFrame;
+
+    if (!cap.open(videoPath))
+    {
+        return false;
+    }
+
+    if (!cap.read(firstFrame))
+    {
+        return false;
+    }
+
+    cvtColor(firstFrame, prevGray, COLOR_BGR2GRAY);
+
+    return true;
+}
+
+bool Preprocessor::getNextFrame(FramePair& frames)
+{
+    Mat currFrame, currGray;
+
+    if (!cap.read(currFrame))
+    {
+        return false;
+    }
+
+    cvtColor(currFrame, currGray, COLOR_BGR2GRAY);
+
+    frames.currGrayFrame = currGray;
+    frames.prevGrayFrame = prevGray;
+
+    prevGray = currGray.clone();
+
+    return true;
+}
