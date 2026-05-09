1413ac83f4ffd2521dce0829f8377aa2d285d907
Qy-wy <lizaliyts@gmail.com>
2026-05-09 21:52:48 +0300

    Implement AttitudeEstimator without visualisation

---
+++ AttitudeEstimator.cpp
@@ -0,0 +1,29 @@
+#include "AttitudeEstimator.h"
+
+using namespace std;
+
+AttitudeEstimator::AttitudeEstimator(const cv::Mat& k, std::string path) : preprocessor(path), matcher(), estimator(k), accumulator(), visualisator(k)
+{
+	k.copyTo(K);
+	videoPath = path;
+}
+
+void AttitudeEstimator::run()
+{
+	if (!preprocessor.loadFirstFrame())
+	{
+		cerr << "Error loading the first frame";
+	}
+
+	FramePair frames;
+
+	while (preprocessor.getNextFrame(frames))
+	{
+		MatchResult matches = matcher.match(frames.prevGrayFrame, frames.currGrayFrame);
+		AnglesIncrements incs = estimator.estimate(matches.point1, matches.point2, matches.matches);
+		accumulator.updateAngles(incs.dteta, incs.dphi, incs.dpsi);
+		double roll = accumulator.getPhi();
+		double pitch = accumulator.getTeta();
+		double yaw = accumulator.getPsi();
+	}
+}
